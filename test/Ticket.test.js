4431ba23e687e433a577c5bd0643ac1af0842b7a
D1scoDancer <apshulikov@edu.hse.ru>
2022-11-13 13:16:52 +0300

    added test

--- test/Ticket.test.js
+++ test/Ticket.test.js
@@ -1,2 +1,20 @@
 const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
+
+describe("Ticket", async function () {
+    let ticket
+    let deployer
+    const initialSupply = 100
+    beforeEach(async function () {
+        deployer = (await getNamedAccounts()).deployer
+        const ticketFactory = await ethers.getContractFactory("Ticket")
+        ticket = await ticketFactory.deploy(initialSupply)
+    })
+
+    describe("Constructor", async function () {
+        it("deployer has LTC tokens", async function () {
+            const response = await ticket.balanceOf(deployer)
+            assert.equal(response, initialSupply)
+        })
+    })
+})
