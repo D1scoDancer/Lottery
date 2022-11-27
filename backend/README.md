0390a4429c797df2d9bf5e2d6b88c243bf5be914
D1scoDancer <apshulikov@edu.hse.ru>
2022-11-27 18:23:14 +0300

    monorep structure + added react

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# Lottery
+
+Commands made:
+
+- yarn init
+- yarn add hardhat --dev
+- yarn hardhat
+- yarn add --dev @nomicfoundation/hardhat-toolbox @nomicfoundation/hardhat-network-helpers @nomicfoundation/hardhat-chai-matchers @nomiclabs/hardhat-ethers @nomiclabs/hardhat-etherscan chai ethers hardhat-gas-reporter solidity-coverage @typechain/hardhat typechain @typechain/ethers-v5 @ethersproject/abi @ethersproject/providers
+- yarn add --dev dotenv
+- yarn add --dev --exact prettier
---
+++ backend/test/Ticket.test.js
@@ -0,0 +1,177 @@
+const { assert, expect } = require("chai")
+const { deployments, ethers, getNamedAccounts } = require("hardhat")
+
+describe("Ticket", async function () {
+    let ticket
+    let deployer
+    let user
+    const initialSupply = 100
+    beforeEach(async function () {
+        deployer = (await getNamedAccounts()).deployer
+        user = (await getNamedAccounts()).user
+        const ticketFactory = await ethers.getContractFactory("Ticket")
+        ticket = await ticketFactory.deploy(initialSupply)
+    })
+
+    describe("Constructor", async function () {
+        it("deployer has LTC tokens", async function () {
+            const response = await ticket.balanceOf(deployer)
+            assert.equal(response, initialSupply)
+        })
+
+        it("user doesn't have LTC tokens", async function () {
+            const response = await ticket.balanceOf(user)
+            assert.equal(response, 0)
+        })
+    })
+
+    describe("buyTickets", async function () {
+        let account
+        beforeEach(async function () {
+            const accounts = await ethers.getSigners()
+            account = accounts[1]
+        })
+
+        it("require msg.value == 0.01 ether", async function () {
+            await expect(
+                ticket.buyTickets({ value: ethers.utils.parseEther("0.001") })
+            ).to.be.revertedWith("1 ticket is 0.01 eth")
+        })
+
+        it("user buys 1 ticket for 0.01 eth", async function () {
+            const ticketConnectedContract = await ticket.connect(account)
+            await ticketConnectedContract.buyTickets({
+                value: ethers.utils.parseEther("0.01"),
+            })
+            const response = await ticket.balanceOf(account.address)
+            assert.equal(response, 1)
+
+            const balance = await ticket.provider.getBalance(ticket.address)
+            assert.equal(
+                balance.toString(),
+                ethers.utils.parseEther("0.01").toString()
+            )
+        })
+
+        it("user buys 2 tickets for 0.02 eth", async function () {
+            const initial_balance = await ticket.provider.getBalance(
+                account.address
+            )
+
+            const ticketConnectedContract = await ticket.connect(account)
+
+            await expect(
+                (txResponse = await ticketConnectedContract.buyTickets({
+                    value: ethers.utils.parseEther("0.02"),
+                }))
+            )
+                .to.emit(ticketConnectedContract, "BuyTickets")
+                .withArgs(2)
+
+            const txReceipt = await txResponse.wait()
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+            const balance = await ticket.provider.getBalance(account.address)
+
+            const expected = initial_balance
+                .sub(gasCost)
+                .sub(ethers.utils.parseEther("0.02"))
+
+            assert.equal(expected.toString(), balance.toString())
+        })
+
+        it("user gets a change after buying 1 ticket for 0.015 eth", async function () {
+            const initial_balance = await ticket.provider.getBalance(
+                account.address
+            )
+
+            const ticketConnectedContract = await ticket.connect(account)
+
+            await expect(
+                (txResponse = await ticketConnectedContract.buyTickets({
+                    value: ethers.utils.parseEther("0.015"),
+                }))
+            )
+                .to.emit(ticketConnectedContract, "BuyTickets")
+                .withArgs(1)
+
+            const txReceipt = await txResponse.wait()
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+            const balance = await ticket.provider.getBalance(account.address)
+            const expected = initial_balance
+                .sub(gasCost)
+                .sub(ethers.utils.parseEther("0.01"))
+
+            assert.equal(expected.toString(), balance.toString())
+        })
+    })
+
+    describe("BuyNumberOfTickets", async function () {
+        let account
+        beforeEach(async function () {
+            const accounts = await ethers.getSigners()
+            account = accounts[1]
+        })
+
+        it("user buys 11 tickets for 0.11 eth", async function () {
+            const initial_balance = await ticket.provider.getBalance(
+                account.address
+            )
+
+            const ticketConnectedContract = await ticket.connect(account)
+
+            await expect(
+                (txResponse = await ticketConnectedContract.BuyNumberOfTickets(
+                    11,
+                    {
+                        value: ethers.utils.parseEther("0.11"),
+                    }
+                ))
+            )
+                .to.emit(ticketConnectedContract, "BuyTickets")
+                .withArgs(11)
+
+            const txReceipt = await txResponse.wait()
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+            const balance = await ticket.provider.getBalance(account.address)
+
+            const expected = initial_balance
+                .sub(gasCost)
+                .sub(ethers.utils.parseEther("0.11"))
+
+            assert.equal(expected.toString(), balance.toString())
+        })
+
+        it("user gets a change after buying 10 ticket for 1 eth", async function () {
+            const initial_balance = await ticket.provider.getBalance(
+                account.address
+            )
+
+            const ticketConnectedContract = await ticket.connect(account)
+
+            await expect(
+                (txResponse = await ticketConnectedContract.BuyNumberOfTickets(
+                    10,
+                    {
+                        value: ethers.utils.parseEther("1"),
+                    }
+                ))
+            )
+                .to.emit(ticketConnectedContract, "BuyTickets")
+                .withArgs(10)
+
+            const txReceipt = await txResponse.wait()
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+            const balance = await ticket.provider.getBalance(account.address)
+
+            const expected = initial_balance
+                .sub(gasCost)
+                .sub(ethers.utils.parseEther("0.1"))
+
+            assert.equal(expected.toString(), balance.toString())
+        })
+    })
+})
