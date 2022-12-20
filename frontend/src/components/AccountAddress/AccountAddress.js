92b8995386b712a9601716558863569cc0161c6d
D1scoDancer <apshulikov@edu.hse.ru>
2022-12-20 18:53:43 +0300

    new structure

---
+++ frontend/src/components/AccountAddress/AccountAddress.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AccountAddress = () => {
+    return (
+        <div>
+            <h3>AccountAddress</h3>
+        </div>
+    )
+}
+
+export default AccountAddress
---
+++ frontend/src/components/AccountBalance/AccountBalance.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AccountBalance = () => {
+    return (
+        <div>
+            <h3>AccountBalance</h3>
+        </div>
+    )
+}
+
+export default AccountBalance
