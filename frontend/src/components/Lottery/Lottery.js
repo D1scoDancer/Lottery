59c5544d2a27b92b0255945fa9b6f2b6eadf8d9d
D1scoDancer <apshulikov@edu.hse.ru>
2023-01-08 11:01:27 +0300

    enterLottery() done

--- frontend/src/components/Lottery/Lottery.js
+++ frontend/src/components/Lottery/Lottery.js
@@ -6,10 +6,32 @@ import listenForTxMine from "../../utils/ListenForTxMine"
 const ethers = require("ethers")
 
 const Lottery = () => {
+    const [lotteryInput, setLotteryInput] = useState(0.001)
     const [numPlayers, setNumPlayers] = useState("?")
     const [fee, setFee] = useState(0)
 
-    const enterLotteryHandler = () => {}
+    const enterLotteryHandler = async (eth_value) => {
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            const signer = provider.getSigner()
+            const contract = new ethers.Contract(
+                contractLotteryAddress,
+                LotteryAbi,
+                signer
+            )
+            try {
+                const txResponse = await contract.enterLottery({
+                    value: ethers.utils.parseEther(lotteryInput.toString()),
+                })
+                await listenForTxMine(txResponse, provider)
+                console.log("Entered lottery!")
+            } catch (error) {
+                console.log(error)
+            }
+        } else {
+            console.log("Install MetaMask")
+        }
+    }
 
     const finishLotteryHandler = () => {}
 
@@ -58,7 +80,13 @@ const Lottery = () => {
     return (
         <div className="lottery">
             <div className="enterLottery">
-                <input type="number" />
+                <input
+                    type="number"
+                    value={lotteryInput}
+                    onInput={(e) => {
+                        setLotteryInput(e.target.value)
+                    }}
+                />
                 <button onClick={enterLotteryHandler} className="storage">
                     Enter Lottery
                 </button>
