6065a418e15a6e9456ecb76d59420e5310c60ee4
D1scoDancer <apshulikov@edu.hse.ru>
2023-01-10 12:18:07 +0300

    helper-hardhat-config done

--- backend/helper-hardhat-config.js
+++ backend/helper-hardhat-config.js
@@ -1,1 +1,27 @@
 const { ethers } = require("hardhat")
+
+const networkConfig = {
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+        // entranceFee: ethers.utils.parseEther("0.001"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "8426",
+        callbackGasLimit: "500000", // its random !CHANGE!
+        // interval: 60,
+    },
+    31337: {
+        name: "hardhat",
+        // entranceFee: ethers.utils.parseEther("10"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // doesn't matter in localhost
+        callbackGasLimit: "500000",
+        // interval: 60,
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
