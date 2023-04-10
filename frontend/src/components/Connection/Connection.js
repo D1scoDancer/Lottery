ec1528d04469daf6a567753f08ea929b309e7a4b
D1scoDancer <apshulikov@edu.hse.ru>
2023-04-10 10:22:04 +0300

    fixed annoying spinner

--- frontend/src/components/Connection/Connection.js
+++ frontend/src/components/Connection/Connection.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { ConnectKitButton, ChainIcon } from "connectkit"
-import { useNetwork } from "wagmi"
+import { useAccount, useNetwork } from "wagmi"
 import "./Connection.css"
 
 const Connection = () => {
     const { chain } = useNetwork()
+    const { isConnected } = useAccount()
 
     return (
         <div className="connection">
-            <ChainIcon id={chain?.id} unsupported={chain?.unsupported} />
+            {isConnected ? <ChainIcon id={chain?.id} unsupported={chain?.unsupported} /> : <></>}
             <label className="name">{chain?.name}</label>
             <ConnectKitButton showAvatar={false} />
         </div>
