8c6eec0b83f06b2c604edf94d04f29946ba996e3
D1scoDancer <apshulikov@edu.hse.ru>
2023-05-01 17:28:09 +0300

    good version now

--- frontend/src/components/Infoblock/Infoblock.js
+++ frontend/src/components/Infoblock/Infoblock.js
@@ -86,9 +86,9 @@ const Infoblock = ({ totalStake, setTotalStake }) => {
                 PrizePool right now is: {ethers.utils.formatEther(totalStake)} MATIC{" "}
             </h4>
             <h5> The prize will be: {prize()} MATIC </h5>
-            {/* <h6 style={{ marginTop: 20 + "px" }}>
+            <h6 style={{ marginTop: 20 + "px" }}>
                 The winner will be decided in {"5 days 14 hours 15 minutes and 45 seconds"}
-            </h6> */}
+            </h6>
         </div>
     )
 }
