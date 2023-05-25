40db106ce3bb5633964d4a55ff5493883da913ad
D1scoDancer <apshulikov@edu.hse.ru>
2023-05-25 19:24:38 +0300

    logo done

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from "react"
 import Connection from "../Connection/Connection"
 import "./Header.css"
+import { Image } from "react-bootstrap"
 
 const Header = () => {
     return (
         <header>
-            <div className="leftpart"></div>
+            <div className="leftpart">
+                <Image
+                    src={process.env.PUBLIC_URL + "/psps_logo.png"}
+                    width={180}
+                    style={{ marginLeft: 20 + "px" }}
+                />
+            </div>
             <div className="rightpart">
                 <Connection />
             </div>
