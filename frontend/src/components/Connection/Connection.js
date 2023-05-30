681bbecbf041bb53ff18663ca01a4a5b4a09cb00
D1scoDancer <apshulikov@edu.hse.ru>
2023-05-30 14:46:39 +0300

    tooltips for wallet

--- frontend/src/components/Connection/Connection.js
+++ frontend/src/components/Connection/Connection.js
@@ -2,15 +2,28 @@ import React from "react"
 import { ConnectKitButton, ChainIcon } from "connectkit"
 import { useAccount, useNetwork } from "wagmi"
 import "./Connection.css"
+import { Tooltip, OverlayTrigger } from "react-bootstrap"
 
 const Connection = () => {
     const { chain } = useNetwork()
     const { isConnected } = useAccount()
 
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            Currently, the supported chain is limited to Polygon Mumbai
+        </Tooltip>
+    )
+
     return (
         <div className="connection">
             {isConnected ? <ChainIcon id={chain?.id} unsupported={chain?.unsupported} /> : <></>}
-            <label className="name">{chain?.name}</label>
+            <OverlayTrigger
+                placement="bottom"
+                delay={{ show: 150, hide: 150 }}
+                overlay={renderTooltip}
+            >
+                <label className="name">{chain?.name}</label>
+            </OverlayTrigger>
             <ConnectKitButton showAvatar={false} />
         </div>
     )
